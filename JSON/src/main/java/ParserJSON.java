6a20827a74db9f9667b9550c4b96120b17118c67
SuranovAN <syranov.a.n.1993@gmail.com>
2020-07-27 20:13:13 +0300

    improve structure

--- JSON/src/main/java/ParserJSON.java
+++ JSON/src/main/java/ParserJSON.java
@@ -1,7 +1,9 @@
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -11,5 +13,19 @@ public class ParserJSON {
         Gson gson = gsonBuilder.create();
         Type listType = new TypeToken<List<T>>(){}.getType();
         return gson.toJson(list, listType);
+    }
+
+    public static void convertToJSON(String JSONData, String fileName){
+        File jsonFile = new File(fileName);
+        try(FileWriter fw = new FileWriter(jsonFile)) {
+            jsonFile.createNewFile();
+            fw.write(JSONData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readString(String s) {
+        return null;
     }
 }
